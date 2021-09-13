c64bc041f880b48e2185debed01c4518cf56e74a
Guilherme Hasselmann <guilhermeluckfett@gmail.com>
2021-09-13 19:36:07 -0300

    implementing charts

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from flask import Flask, render_template,request,jsonify
-from bson import ObjectId, json_util
+from bson import json_util
 from pymongo import MongoClient
 import os
 import qrcode
 import ast
 from datetime import datetime, timedelta
+import base64
 
 app = Flask(__name__)
 
 qr_images = os.path.join('static', 'images')
 app.config['QRIMAGES'] = qr_images
 
-client = MongoClient(os.environ['CUSTOMCONNSTR_DBCONNECTION'])
+client = MongoClient("mongodb://dbcovid:jJKZMcJ1Uhh0kVVzVDXCubicG6UBkLreHJcR4uY830VRAEFtk3Tw0w6RJn7xm93wDEiaMFj9ukJIbIQIgg62rg==@dbcovid.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@dbcovid@")
+#client = MongoClient(os.environ['CUSTOMCONNSTR_DBCONNECTION'])
 db = client.mymongodb
-todos = db.coviddata
+dados_viagem = db.coviddata
 
-BASE_URL = "https://covid19ufpr.azurewebsites.net/get/"
+BASE_URL = "https://127.0.0.1:5000/get/"
+#BASE_URL = "https://covid19ufpr.azurewebsites.net/get/"
 
 @app.route('/', methods=['GET'])
 def main_page():
@@ -29,28 +32,47 @@ def post_data(id):
     temperatura=data_received["temperatura"]
     umidade = data_received["umidade"]
     luminosidade = data_received["luminosidade"]
-    todos.insert({"hashid": id, "periodo": dt_string,"temperatura": temperatura, "umidade": umidade, "luminosidade": luminosidade})
+    dados_viagem.insert({"hashid": id, "periodo": dt_string,"temperatura": temperatura, "umidade": umidade, "luminosidade": luminosidade})
     return jsonify({"status": "OK", "id": id, "periodo":dt_string, "temperatura": temperatura, "umidade": umidade, "luminosidade": luminosidade})
 
 
 @app.route('/get/<id>', methods=['GET'])
 def get_data(id):
     elements_list = []
-    data_return = todos.find({"hashid":id})
+    period_list = []
+    temperature_list = []
+    humidity_list = []
+    luminosity_list = []
+    data_return = dados_viagem.find({"hashid":id})
 
     data = json_util.dumps(data_return, default=json_util.default)
     data_converted = ast.literal_eval(data)
     for element in data_converted:
         elements_list.append(element['periodo'])
+        period_list.append(element['periodo'])
         elements_list.append(element['temperatura'])
+        temperature_list.append(element['temperatura'])
         elements_list.append(element['umidade'])
+        humidity_list.append(element['umidade'])
         elements_list.append(element['luminosidade'])
+        luminosity_list.append(element['luminosidade'])
 
-    return render_template('data.html', title='UFPR COVID MONITOR', dados=elements_list)
+    decoded_data = base64.b64decode(id)
+    dict_data = ast.literal_eval(decoded_data.decode())
+
+    return render_template('data.html', title='UFPR COVID MONITOR', origem=dict_data["origem"], 
+    destino=dict_data["destino"], idLote=dict_data["idLote"], dados=elements_list, periodo=period_list,
+    temperatura=temperature_list, umidade=humidity_list, luminosidade=luminosity_list)
 
 @app.route('/qrcode', methods=['POST'])
 def get_qrcode():
-    hash_generated = str(hash(request.values.get("destino") + request.values.get("origem")))
+    data_generated = str({
+        "origem":f"{request.values.get('origem')}",
+        "destino":f"{request.values.get('destino')}",
+        "idLote":f"{request.values.get('idLote')}"
+    })
+    data_generated_bytes = data_generated.encode("ascii")
+    hash_generated = (base64.b64encode(data_generated_bytes)).decode("ascii")
     img = qrcode.make(f'{BASE_URL}{hash_generated}')
     img.save(f'{hash_generated}.png')
     os.rename(f'{hash_generated}.png', f'./static/images/{hash_generated}.png')
