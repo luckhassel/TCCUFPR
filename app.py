7c1b9ce322f27326042106ee9e31e800d01e003a
luckhassel <36700286+luckhassel@users.noreply.github.com>
2021-09-14 17:51:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,14 +16,13 @@ app.config['SECRET_KEY'] = 'oh_so_secret'
 qr_images = os.path.join('static', 'images')
 app.config['QRIMAGES'] = qr_images
 
-client = MongoClient("mongodb://dbcovid:jJKZMcJ1Uhh0kVVzVDXCubicG6UBkLreHJcR4uY830VRAEFtk3Tw0w6RJn7xm93wDEiaMFj9ukJIbIQIgg62rg==@dbcovid.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@dbcovid@")
-#client = MongoClient(os.environ['CUSTOMCONNSTR_DBCONNECTION'])
+client = MongoClient(os.environ['CUSTOMCONNSTR_DBCONNECTION'])
 db = client.mymongodb
 dados_viagem = db.coviddata
 dados_pessoas = db.peopledata
 
-BASE_URL = "https://127.0.0.1:5000/get/"
-#BASE_URL = "https://covid19ufpr.azurewebsites.net/get/"
+#BASE_URL = "https://127.0.0.1:5000/get/"
+BASE_URL = "https://covid19ufpr.azurewebsites.net/get/"
 
 pwd_context = CryptContext(
         schemes=["pbkdf2_sha256"],
